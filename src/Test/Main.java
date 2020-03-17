98f32614f1803d0061391a1788a15ca4df85b869
user <user@user-PC>
2020-03-17 14:49:23 +0900

    # WARNING: head commit changed in the meantime
    03 17 jeayeon

---
+++ src/Test/Main.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		System.out.println(a+b);
+		int c = a+b;
+		System.out.println(c);
+
+	}
+
+}
